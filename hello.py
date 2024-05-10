3029c4a97a4bf07f5ad49e4a4d400bf71b3f8009
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-05-10 20:53:03 +0100

    430. Advanced Decorators with *args and **kwargs

--- hello.py
+++ hello.py
@@ -1,13 +1,34 @@
 from flask import Flask
 app = Flask(__name__)
 
+def make_bold(funct):
+    def wrapper_function(*args, **kwargs):
+        val = f"<b>{funct(*args, **kwargs)}<b>"
+        return val
+    return wrapper_function
+
+def make_italic(funct):
+    def wrapper_function(*args, **kwargs):
+        val = f"<em>{funct(*args, **kwargs)}<em>"
+        return val
+    return wrapper_function
+
+def make_underline(funct):
+    def wrapper_function(*args, **kwargs):
+        val = f"<u>{funct(*args, **kwargs)}<u>"
+        return val
+    return wrapper_function
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/bye")
-def bye():
-    return "<p>bye</p>"
+@make_bold
+@make_italic
+@make_underline
+def bye(greet="bye"):
+    return f"{greet}"
 
 @app.route("/<name>")
 def greet(name):
